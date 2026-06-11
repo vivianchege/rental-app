6eaa91953d3e23cb08454060352ee24e117515fc
chainz254 <pngikunju671@gmail.com>
2026-06-11 16:08:47 -0700

    fix: clear comments from main.jsx imports to restore styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './App.css' // <-- THIS IS THE MAGIC LINE
+import './index.css' // <-- THIS IS THE MAGIC LINE
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
